ae2e872c35834d55561ee4b1805204304b989f7e
Cyatos <yashdc@gmail.com>
2015-02-05 00:10:57 +0530

    Speed Pickup
    1. Recoded picking mechanism in javascript
    2. Implemented Speed Pickup

---
+++ ImBadAtNamingThings/Assets/Scripts/Picker.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+function OnTriggerEnter(other : Collider)
+  {
+   if(other.gameObject.tag == ("PickUp"))
+    {
+     Destroy(other.gameObject);
+     CharacterMotorMovement.maxForwardSpeed +=10000;
+     Debug.Log("0");
+     yield WaitForSeconds(10);
+     CharacterMotorMovement.maxForwardSpeed -=10000;
+     Debug.Log("10");
+    }
+   }
+     
