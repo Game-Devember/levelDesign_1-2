854489d4a7b07066f722727c41c31dcb5e3b664a
Cyatos <yashdc@gmail.com>
2015-02-14 11:52:30 +0530

    Gave Apocalyptic feel, again

---
+++ ImBadAtNamingThings/Assets/script.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () 
+  {
+
+        if ( Random.value > 0.9 ) //a random chance
+        {
+           if ( light.enabled == true ) //if the light is on...
+           {
+             light.enabled = false; //turn it off
+           }
+           else
+           {
+             light.enabled = true; //turn it on
+           }
+        }
+    }
+
+
