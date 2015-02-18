9b4efc19be5e68517fbe6a18b915acd07378e4a4
Cyatos <yashdc@gmail.com>
2015-02-18 17:12:05 +0530

    Health System

---
+++ ImBadAtNamingThings/Assets/Health.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+var health =100.0;
+
+function Start () {
+
+}
+
+function Update () 
+{
+if(health<=0)
+ Destroy(gameObject);
+}
+function OnTriggerEnter(other : Collider)
+  {
+   if(other.gameObject.tag==("Zombie"))
+    {
+     health-=25;
+    }
+  }
+  
