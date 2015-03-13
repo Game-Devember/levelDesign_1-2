c62191c54777ecef2d5cc4722093b6cb18b42829
Cyatos <yashdc@gmail.com>
2015-03-14 02:42:13 +0530

    1.7.3a

--- ImBadAtNamingThings/Assets/WalkSpeedController.js
+++ ImBadAtNamingThings/Assets/WalkSpeedController.js
@@ -1,9 +1,14 @@
 ﻿#pragma strict
-
+var player :Transform;
 function Start () {
 animation["Take 001"].speed=1.5;
 }
 
 function Update () {
-
+if(Input.GetAxis("Vertical"))
+{
+animation.CrossFade("Take 001");
+}
+else
+animation.Stop("Take 001");
 }
