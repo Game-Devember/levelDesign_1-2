ea403060c0072a6660447e207dd70d6008a24843
Cyatos <yashdc@gmail.com>
2015-03-15 22:07:53 +0530

    Version 1.8.5

--- ImBadAtNamingThings/Assets/Scripts/Picker.js
+++ ImBadAtNamingThings/Assets/Scripts/Picker.js
@@ -1,5 +1,6 @@
 ﻿#pragma strict
 var SpawnPoint:Transform;
+var markers :Transform;
 function Start () {
 yield WaitForSeconds(5);
 transform.position=SpawnPoint.position;
@@ -12,14 +13,17 @@ function Update () {
 function OnTriggerEnter(other : Collider)
   {
    if(other.gameObject.tag == ("PickUp"))
-    {
+    { 
+     markers.active=true;
      Destroy(other.gameObject);
      CharacterMotorMovement.maxForwardSpeed +=20;
      CharacterMotorMovement.maxSidewaysSpeed+=20;
      CharacterMotorMovement.maxBackwardsSpeed+=20;
      CharacterMotorMovement.maxGroundAcceleration+=20;
      Debug.Log("0");
+     
      yield WaitForSeconds(10);
+     markers.active=false;
      CharacterMotorMovement.maxForwardSpeed -=20;
      CharacterMotorMovement.maxSidewaysSpeed -=20;
      CharacterMotorMovement.maxBackwardsSpeed -=20;
--- ImBadAtNamingThings/Assets/Scripts/WalkSpeedController.js
+++ ImBadAtNamingThings/Assets/Scripts/WalkSpeedController.js
@@ -1,11 +1,16 @@
 ﻿#pragma strict
 var player :Transform;
+var movement:MonoScript;
+var speed :float;
+
 function Start () {
 animation.CrossFade("idle");
 animation["Take 001"].speed=1.5;
+
 }
 
 function Update () {
+  animation["Take 001"].speed=1.5;
 if(Input.GetAxis("Vertical"))
 {
 animation.CrossFade("Take 001");
