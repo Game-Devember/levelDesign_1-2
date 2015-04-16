ed483a2f76c193b6d1d545cb81377183c152d5d1
Sahil Singh <sahilsingh10394@gmail.com>
2015-04-16 20:37:54 +0530

    V 1.2.0
    Animations

---
+++ ImBadAtNamingThings/Assets/animControl.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+private var anim : Animator;
+private var v : float;
+private var aim: float;
+
+function Start () {
+
+anim = GetComponent("Animator");
+anim.SetFloat("idleShoot",0);
+}
+
+function Update () {
+
+	v = Input.GetAxis("Vertical");
+	anim.SetFloat("Speed",v);
+	if(Input.GetMouseButtonDown(0)) playIdle();
+}
+
+function playIdle()
+{
+	aim = 0.01;
+	while(aim < 1)
+	{
+		anim.SetFloat("idleShoot",aim);
+		yield WaitForSeconds(0.01);
+		aim += 0.09;
+	}
+	anim.SetFloat("idleShoot",1);
+	yield WaitForSeconds(0.3);
+	while(aim > 0)
+	{
+		anim.SetFloat("idleShoot",aim);
+		yield WaitForSeconds(0.01);
+		aim -= 0.05;
+	}
+	anim.SetFloat("idleShoot",0);	
+}
